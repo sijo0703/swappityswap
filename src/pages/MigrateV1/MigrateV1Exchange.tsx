69f507153b016efa3eafdcfb4bc659fc9f4663b4
Lint Action <lint-action@samuelmeuli.com>
2022-01-15 22:56:22 +0000

    Fix code style issues with ESLint

--- src/pages/MigrateV1/MigrateV1Exchange.tsx
+++ src/pages/MigrateV1/MigrateV1Exchange.tsx
@@ -1,15 +1,6 @@
 import { TransactionResponse } from '@ethersproject/abstract-provider'
 import { AddressZero } from '@ethersproject/constants'
-import {
-  Currency,
-  CurrencyAmount,
-  Fraction,
-  JSBI,
-  Percent,
-  Token,
-  TokenAmount,
-  WETH
-} from '@sijo0703/swappityswap'
+import { Currency, CurrencyAmount, Fraction, JSBI, Percent, Token, TokenAmount, WETH } from '@sijo0703/swappityswap'
 import React, { useCallback, useMemo, useState } from 'react'
 import ReactGA from 'react-ga'
 import { Redirect, RouteComponentProps } from 'react-router'
